a1eae653f2b92932c08822b416430c12f5feb74d
nobe4 <nobe4@users.noreply.github.com>
2024-05-19 18:51:05 +0200

    feat: add more actors

--- internal/actors/actors.go
+++ internal/actors/actors.go
@@ -3,21 +3,31 @@ package actors
 import (
 	"fmt"
 
+	"github.com/nobe4/gh-not/internal/gh"
 	"github.com/nobe4/gh-not/internal/notifications"
 )
 
 type ActorsMap map[string]Actor
 
-func Map() ActorsMap {
+func Map(client *gh.Client) ActorsMap {
 	return map[string]Actor{
+		"pass":  &PassActor{},
 		"debug": &DebugActor{},
 		"print": &PrintActor{},
 		"hide":  &HideActor{},
+		"read":  &ReadActor{client: client},
+		"done":  &DoneActor{client: client},
 	}
 }
 
 type Actor interface {
 	Run(notifications.Notification) (notifications.Notification, error)
+}
+
+type PassActor struct{}
+
+func (_ *PassActor) Run(n notifications.Notification) (notifications.Notification, error) {
+	return n, nil
 }
 
 type DebugActor struct{}
@@ -41,7 +51,44 @@ func (_ *PrintActor) Run(n notifications.Notification) (notifications.Notificati
 type HideActor struct{}
 
 func (_ *HideActor) Run(n notifications.Notification) (notifications.Notification, error) {
-	// fmt.Printf("HIDDING %s\n", n.ToString())
 	n.Meta.Hidden = !n.Meta.Hidden
 	return n, nil
+}
+
+// Actor that marks a notification as read.
+// Ref: https://docs.github.com/en/rest/activity/notifications?apiVersion=2022-11-28#mark-a-thread-as-read
+type ReadActor struct {
+	client *gh.Client
+}
+
+func (a *ReadActor) Run(n notifications.Notification) (notifications.Notification, error) {
+	err := a.client.API.Patch(n.URL, nil, nil)
+
+	// go-gh currently fails to handle HTTP-205 correctly, however it's possible
+	// to catch this case.
+	// ref: https://github.com/cli/go-gh/issues/161
+	if err != nil && err.Error() != "unexpected end of JSON input" {
+		return n, err
+	}
+
+	n.Unread = false
+
+	return n, nil
+}
+
+// Actor that marks a notification as done.
+// Ref: https://docs.github.com/en/rest/activity/notifications?apiVersion=2022-11-28#mark-a-thread-as-done
+type DoneActor struct {
+	client *gh.Client
+}
+
+func (a *DoneActor) Run(n notifications.Notification) (notifications.Notification, error) {
+	emptyNotification := notifications.Notification{}
+
+	err := a.client.API.Delete(n.URL, nil)
+	if err != nil {
+		return emptyNotification, err
+	}
+
+	return emptyNotification, nil
 }
