5f18df0df54ac1198c7b954fd4a482d1ca7df502
nobe4 <nobe4@users.noreply.github.com>
2024-05-22 18:54:48 +0200

    feat(cmd/list): add examples
    Also move RunE to a function

--- internal/cmd/list.go
+++ internal/cmd/list.go
@@ -13,37 +13,14 @@ var (
 	jqFlag     = ""
 
 	listCmd = &cobra.Command{
-		Use:   "list",
-		Short: "List notifications",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			notifications, err := client.Notifications()
-			if err != nil {
-				slog.Error("Failed to list notifications", "err", err)
-				return err
-			}
-
-			if filterFlag != "" {
-				notificationsList, err := jq.Filter(filterFlag, notifications.ToSlice())
-				if err != nil {
-					return err
-				}
-				notifications = notificationsList.ToMap()
-			}
-
-			if jqFlag != "" {
-				return fmt.Errorf("`gh-not list --jq` implementation needed")
-			}
-
-			out, err := notifications.ToTable()
-			if err != nil {
-				slog.Warn("Failed to generate a table, using toString", "err", err)
-				out = notifications.ToString()
-			}
-
-			fmt.Println(out)
-
-			return nil
-		},
+		Use:     "list",
+		Aliases: []string{"ls"},
+		Short:   "List notifications",
+		Example: `
+  gh-not list
+  gh-not list --filter ''.author.login | contains("4")'
+`,
+		RunE: runList,
 	}
 )
 
@@ -53,4 +30,34 @@ func init() {
 	listCmd.Flags().StringVarP(&filterFlag, "filter", "f", "", "Filter with a jq expression passed into a select(...) call")
 	listCmd.Flags().StringVarP(&jqFlag, "jq", "q", "", "jq expression to run on the notification list")
 	listCmd.MarkFlagsMutuallyExclusive("filter", "jq")
+}
+
+func runList(cmd *cobra.Command, args []string) error {
+	notifications, err := client.Notifications()
+	if err != nil {
+		slog.Error("Failed to list notifications", "err", err)
+		return err
+	}
+
+	if filterFlag != "" {
+		notificationsList, err := jq.Filter(filterFlag, notifications.ToSlice())
+		if err != nil {
+			return err
+		}
+		notifications = notificationsList.ToMap()
+	}
+
+	if jqFlag != "" {
+		return fmt.Errorf("`gh-not list --jq` implementation needed")
+	}
+
+	out, err := notifications.ToTable()
+	if err != nil {
+		slog.Warn("Failed to generate a table, using toString", "err", err)
+		out = notifications.ToString()
+	}
+
+	fmt.Println(out)
+
+	return nil
 }
