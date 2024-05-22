9a05a7d03101abf34602eccef26867fcc92d77f2
nobe4 <nobe4@users.noreply.github.com>
2024-05-22 18:57:59 +0200

    refactor(cmd/config): move RunE in function

--- internal/cmd/config.go
+++ internal/cmd/config.go
@@ -12,21 +12,23 @@ var (
 	configCmd = &cobra.Command{
 		Use:   "config",
 		Short: "Show configuration information",
-		RunE: func(cmd *cobra.Command, args []string) error {
-
-			marshalled, err := yaml.Marshal(config)
-			if err != nil {
-				slog.Error("Failed to marshall config", "err", err)
-			}
-
-			fmt.Println(configPathFlag)
-			fmt.Println(string(marshalled))
-
-			return nil
-		},
+		RunE:  runConfig,
 	}
 )
 
 func init() {
 	rootCmd.AddCommand(configCmd)
+}
+
+func runConfig(cmd *cobra.Command, args []string) error {
+
+	marshalled, err := yaml.Marshal(config)
+	if err != nil {
+		slog.Error("Failed to marshall config", "err", err)
+	}
+
+	fmt.Println(configPathFlag)
+	fmt.Println(string(marshalled))
+
+	return nil
 }
