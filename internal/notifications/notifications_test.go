e5adb119d1a13d6641c59de6253be0f787e02243
nobe4 <nobe4@users.noreply.github.com>
2024-06-07 15:57:56 -0400

    test(notifications): add id list test

--- internal/notifications/notifications_test.go
+++ internal/notifications/notifications_test.go
@@ -2,6 +2,22 @@ package notifications
 
 import "testing"
 
+func TestIDList(t *testing.T) {
+	n := Notifications{}
+	ids := []string{"0", "1", "2"}
+	for _, id := range ids {
+		n = append(n, &Notification{Id: id})
+	}
+
+	got := n.IDList()
+
+	for i, id := range ids {
+		if id != got[i] {
+			t.Fatalf("expected %+v but got %+v", id, got[i])
+		}
+	}
+}
+
 func TestDeleteNil(t *testing.T) {
 	n0 := &Notification{Id: "0"}
 	n1 := &Notification{Id: "1"}
@@ -13,11 +29,11 @@ func TestDeleteNil(t *testing.T) {
 		t.Fatalf("expected 2 elements but got %d\n%+v", len(got), got)
 	}
 
-    if got[0] != n0 {
+	if got[0] != n0 {
 		t.Fatalf("expected %+v but got %+v", n0, got[0])
-    }
+	}
 
-    if got[1] != n1 {
+	if got[1] != n1 {
 		t.Fatalf("expected %+v but got %+v", n1, got[1])
-    }
+	}
 }
