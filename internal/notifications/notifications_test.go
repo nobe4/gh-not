8b8c3f21c754aed15d2659b9f5eb65f7f421d1c8
nobe4 <nobe4@users.noreply.github.com>
2024-06-10 15:29:46 +0200

    test(notifications): add Uniq test

--- internal/notifications/notifications_test.go
+++ internal/notifications/notifications_test.go
@@ -13,7 +13,7 @@ func TestIDList(t *testing.T) {
 
 	for i, id := range ids {
 		if id != got[i] {
-			t.Fatalf("expected %+v but got %+v", id, got[i])
+			t.Fatalf("expected %d: %s but got %s", i, id, got[i])
 		}
 	}
 }
@@ -35,5 +35,30 @@ func TestDeleteNil(t *testing.T) {
 
 	if got[1] != n1 {
 		t.Fatalf("expected %+v but got %+v", n1, got[1])
+	}
+}
+
+func TestUniq(t *testing.T) {
+	n0 := &Notification{Id: "0"}
+	n1 := &Notification{Id: "1"}
+	n2 := &Notification{Id: "2"}
+	n := Notifications{n0, n1, n2, n0, n1, n2}
+
+	got := n.Uniq()
+
+	if len(got) != 3 {
+		t.Fatalf("expected 3 elements but got %d\n%+v", len(got), got)
+	}
+
+	if got[0] != n0 {
+		t.Fatalf("expected %+v but got %+v", n0, got[0])
+	}
+
+	if got[1] != n1 {
+		t.Fatalf("expected %+v but got %+v", n1, got[1])
+	}
+
+	if got[2] != n2 {
+		t.Fatalf("expected %+v but got %+v", n2, got[2])
 	}
 }
