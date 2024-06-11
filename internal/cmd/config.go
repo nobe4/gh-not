c18539efcce8a19394635c419839c44f23d95714
nobe4 <nobe4@users.noreply.github.com>
2024-06-11 13:00:41 +0200

    feat(config): add edit flag (#24)

--- internal/cmd/config.go
+++ internal/cmd/config.go
@@ -3,12 +3,16 @@ package cmd
 import (
 	"fmt"
 	"log/slog"
+	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
 )
 
 var (
+	editConfigFlag = false
+
 	configCmd = &cobra.Command{
 		Use:   "config",
 		Short: "Show configuration information",
@@ -18,9 +22,15 @@ var (
 
 func init() {
 	rootCmd.AddCommand(configCmd)
+
+	configCmd.Flags().BoolVarP(&editConfigFlag, "edit", "e", false, "Edit the config in $EDITOR")
 }
 
 func runConfig(cmd *cobra.Command, args []string) error {
+	if editConfigFlag {
+		return edit()
+	}
+
 	marshalled, err := yaml.Marshal(config)
 	if err != nil {
 		slog.Error("Failed to marshall config", "err", err)
@@ -28,6 +38,29 @@ func runConfig(cmd *cobra.Command, args []string) error {
 
 	fmt.Println(configPathFlag)
 	fmt.Println(string(marshalled))
+
+	return nil
+}
+
+func edit() error {
+	editor := os.Getenv("EDITOR")
+	if editor == "" {
+		return fmt.Errorf("EDITOR environment variable not set")
+	}
+
+	cmd := exec.Command(editor, configPathFlag)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	if err := cmd.Wait(); err != nil {
+		return err
+	}
 
 	return nil
 }
