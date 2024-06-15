1b98da5a8bbb18ea6e891b5b1d04335a5b775952
nobe4 <nobe4@users.noreply.github.com>
2024-06-15 11:47:44 +0200

    docs(sync): improve sync-related documentation

--- internal/notifications/sync.go
+++ internal/notifications/sync.go
@@ -6,13 +6,15 @@ package notifications
 // | remote \ local | Missing   | Exist     | ToDelete  |
 // | ---            | ---       | ---       | ---       |
 // | Exist          | (1)Insert | (2)Update | (2)Update |
-// | Missing        | (3)Noop   | (3)Noop   | (4)Drop   |
+// | Missing        | (3)Keep   | (3)Keep   | (4)Drop   |
 //
 //  1. Insert: Add the notification ass is.
 //  2. Update: Update the local notification with the remote data, keep the Meta
 //     unchanged.
-//  3. Noop: Do nothing.
+//  3. Keep: Keep the local notification unchanged.
 //  4. Drop: Remove the notification from the local list.
+//
+// TODO: refactor this to `func (n Notifications) Sync(remote Notifications) {}`
 func Sync(local, remote Notifications) Notifications {
 	remoteMap := remote.Map()
 	localMap := local.Map()
@@ -40,7 +42,7 @@ func Sync(local, remote Notifications) Notifications {
 				continue
 			}
 
-			// (3)Noop
+			// (3)Keep
 			n = append(n, local)
 		}
 	}
