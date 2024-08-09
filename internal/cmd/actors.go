3e6084a1dcce47910ab7b8eccc3e072ac3754dd9
nobe4 <nobe4@users.noreply.github.com>
2024-08-10 00:27:58 +0200

    feat(cmd): add actors command to document usage

---
+++ internal/cmd/actors.go
@@ -0,0 +1,30 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var (
+	actorsCmd = &cobra.Command{
+		Use:   "actors",
+		Short: "Show information about the actors",
+		Long: `
+'gh-not' has multiple actors that perform different actions:
+
+open: Open the notification in a web browser.
+
+hide: Mark the notification as hidden in the cache.
+      It won't show the notification again.
+
+done: Mark the notification as done in the cache and on the API.
+      It hides the notification until an update happens.
+
+read: Mark the notification as read in the cache and on the API.
+      It hides the notification until an update happens.
+
+TODO: remove debug/print/pass
+`,
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(actorsCmd)
+}
