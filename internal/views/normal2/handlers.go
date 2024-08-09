46a14ad19cc81ae181002d38d0b7e78b0b1c2760
nobe4 <nobe4@users.noreply.github.com>
2024-08-09 20:29:55 +0200

    refactor(normal): refactor more methods (#128)

--- internal/views/normal2/handlers.go
+++ internal/views/normal2/handlers.go
@@ -12,13 +12,17 @@ import (
 func (m model) handleKeyMsg(msg tea.KeyMsg) (tea.Model, tea.Cmd) {
 	switch {
 	case m.showResult:
-		return m, m.handleResult(msg)
+		return m.handleResult(msg)
+
 	case m.command.Focused():
-		return m, m.handleCommand(msg)
+		return m.handleCommand(msg)
+
 	case m.list.FilterState() == list.Filtering:
-		return m, m.handleFiltering(msg)
+		return m.handleFiltering(msg)
+
 	default:
-		return m, m.handleBrowsing(msg)
+		return m.handleBrowsing(msg)
+
 	}
 }
 
@@ -37,33 +41,21 @@ func (_ CleanListMsg) apply(m model) (tea.Model, tea.Cmd) {
 	return m, m.list.SetItems(items)
 }
 
-func (m *model) handleCommand(msg tea.KeyMsg) tea.Cmd {
-	var cmd tea.Cmd
-
+func (m *model) handleCommand(msg tea.KeyMsg) (tea.Model, tea.Cmd) {
 	switch {
 	case key.Matches(msg, m.keymap.CommandAccept):
-		// TODO: move into a method
-		slog.Debug("blur command")
-		command := m.command.Value()
-		m.command.SetValue("")
-		m.command.Blur()
-		m.showResult = true
-		return m.applyCommand(command)
+		return m.acceptCommand()
 
 	case key.Matches(msg, m.keymap.CommandCancel):
-		// TODO: move into a method
-		slog.Debug("blur command")
-		m.command.SetValue("")
-		m.command.Blur()
-		return nil
+		return m.cancelCommand()
 	}
 
+	var cmd tea.Cmd
 	m.command, cmd = m.command.Update(msg)
-	return cmd
+	return m, cmd
 }
 
-func (m *model) handleResult(msg tea.KeyMsg) tea.Cmd {
-	var cmd tea.Cmd
+func (m *model) handleResult(msg tea.KeyMsg) (tea.Model, tea.Cmd) {
 
 	switch {
 
@@ -74,14 +66,15 @@ func (m *model) handleResult(msg tea.KeyMsg) tea.Cmd {
 
 	case key.Matches(msg, m.list.KeyMap.Quit):
 		m.showResult = false
-		return nil
+		return m, nil
 	}
 
+	var cmd tea.Cmd
 	m.result, cmd = m.result.Update(msg)
-	return cmd
+	return m, cmd
 }
 
-func (m *model) handleBrowsing(msg tea.KeyMsg) tea.Cmd {
+func (m *model) handleBrowsing(msg tea.KeyMsg) (tea.Model, tea.Cmd) {
 	slog.Debug("browsing", "key", msg.String())
 
 	switch {
@@ -94,33 +87,16 @@ func (m *model) handleBrowsing(msg tea.KeyMsg) tea.Cmd {
 			i.selected = !i.selected
 			slog.Debug("toggle selected", "item", i.notification.Subject.Title, "selected", i.selected)
 
-			return m.list.SetItem(m.list.GlobalIndex(), i)
+			return m, m.list.SetItem(m.list.GlobalIndex(), i)
 		}
 
 	case key.Matches(msg, m.keymap.All):
-		// TODO: move into a method
-		items := []list.Item{}
-		for _, e := range m.list.Items() {
-			if i, ok := e.(item); ok {
-				i.selected = true
-				items = append(items, i)
-			}
-		}
-		m.list.SetItems(items)
+		return m.selectAll(true)
 
 	case key.Matches(msg, m.keymap.None):
-		// TODO: move into a method
-		items := []list.Item{}
-		for _, e := range m.list.Items() {
-			if i, ok := e.(item); ok {
-				i.selected = false
-				items = append(items, i)
-			}
-		}
-		m.list.SetItems(items)
+		return m.selectAll(true)
 
 	case key.Matches(msg, m.keymap.Open):
-		// TODO: move into a method
 		current, ok := m.list.SelectedItem().(item)
 		if ok {
 			m.actors["open"].Run(current.notification, os.Stderr)
@@ -128,17 +104,18 @@ func (m *model) handleBrowsing(msg tea.KeyMsg) tea.Cmd {
 
 	case key.Matches(msg, m.keymap.CommandMode):
 		slog.Debug("focus command")
-		m.command.Focus()
+		return m, m.command.Focus()
 	}
 
 	var cmd tea.Cmd
 	m.list, cmd = m.list.Update(msg)
-	return cmd
+	return m, cmd
 }
 
-func (m *model) handleFiltering(msg tea.KeyMsg) tea.Cmd {
+func (m *model) handleFiltering(msg tea.KeyMsg) (tea.Model, tea.Cmd) {
 	slog.Debug("filtering", "key", msg.String())
+
 	var cmd tea.Cmd
 	m.list, cmd = m.list.Update(msg)
-	return cmd
+	return m, cmd
 }
