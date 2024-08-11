4898d36f326e067c749f75a2f9ef285d425b45f4
nobe4 <nobe4@users.noreply.github.com>
2024-08-11 11:40:48 +0200

    fix(hide): improve outputs, don't clean the notification

--- internal/actors/hide/hide.go
+++ internal/actors/hide/hide.go
@@ -1,22 +1,22 @@
 package hide
 
 import (
+	"fmt"
 	"io"
+	"log/slog"
 
+	"github.com/nobe4/gh-not/internal/colors"
 	"github.com/nobe4/gh-not/internal/notifications"
 )
 
 type Actor struct{}
 
-func (_ *Actor) Run(n *notifications.Notification, _ io.Writer) error {
-	n.Meta.Hidden = !n.Meta.Hidden
+func (_ *Actor) Run(n *notifications.Notification, w io.Writer) error {
+	slog.Debug("marking notification as hidden", "notification", n.Id)
 
-	n = &notifications.Notification{
-		Id: n.Id,
-		Meta: notifications.Meta{
-			Hidden: n.Meta.Hidden,
-		},
-	}
+	n.Meta.Hidden = true
+
+	fmt.Fprint(w, colors.Red("HIDE ")+n.String())
 
 	return nil
 }
