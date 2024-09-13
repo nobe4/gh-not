160d6a16c93817559683c0d73d382a6b784c6cf0
nobe4 <nobe4@users.noreply.github.com>
2024-09-13 21:01:24 +0200

    feat(open): add output for open action

--- internal/actions/open/open.go
+++ internal/actions/open/open.go
@@ -4,10 +4,12 @@ Package open implements an [actions.Runner] that opens a notification in the bro
 package open
 
 import (
+	"fmt"
 	"io"
 	"log/slog"
 
 	"github.com/cli/go-gh/pkg/browser"
+	"github.com/nobe4/gh-not/internal/colors"
 	"github.com/nobe4/gh-not/internal/gh"
 	"github.com/nobe4/gh-not/internal/notifications"
 )
@@ -20,5 +22,9 @@ func (a *Runner) Run(n *notifications.Notification, w io.Writer) error {
 	slog.Debug("open notification in browser", "notification", n)
 
 	browser := browser.New("", w, w)
-	return browser.Browse(n.Subject.HtmlUrl)
+
+	err := browser.Browse(n.Subject.HtmlUrl)
+	fmt.Fprint(w, colors.Blue("OPEN ")+n.Subject.URL)
+
+	return err
 }
