9d5f90b6062b5f0ce1d581efe4aaffad4565909d
nobe4 <nobe4@users.noreply.github.com>
2024-09-18 21:17:48 +0200

    fix(errors): update gh errors test language

--- internal/gh/errors_test.go
+++ internal/gh/errors_test.go
@@ -5,9 +5,9 @@ import "testing"
 func TestRetryError(t *testing.T) {
 	e := RetryError{
 		verb: "verb",
-		url:  "endpoint",
+		url:  "url",
 	}
-	want := "retry exceeded for verb endpoint"
+	want := "retry exceeded for verb url"
 	got := e.Error()
 
 	if got != want {
