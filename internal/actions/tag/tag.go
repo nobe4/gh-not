0bb6f7ad3b37a99df0a267a6d24280dd313e9290
nobe4 <nobe4@users.noreply.github.com>
2024-10-05 21:16:43 +0200

    feat(tag): add tag action (#202)

---
+++ internal/actions/tag/tag.go
@@ -0,0 +1,71 @@
+/*
+Package tag implements an [actions.Runner] that manages tags in a notification.
+
+Tags are stored sorted and unique.
+
+It takes as arguments the tags to add prefixed by `+` and the tags to remove
+prefixed by `-`. If a tag is not prefixed, it is added.
+
+E.g.: `tag0 -tag1 +tag2` will add `tag0` and `tag2` and remove `tag1`.
+
+Adding an existing tag is a no-op.
+Removing a missing tag is a no-op.
+
+Usage in the config:
+
+	rules:
+	  - action: tag
+	    args: ["+tag0", "-tag1"]
+
+Usage in the REPL:
+
+	:tag +tag0 -tag1
+*/
+package tag
+
+import (
+	"fmt"
+	"io"
+	"log/slog"
+	"slices"
+
+	"github.com/nobe4/gh-not/internal/colors"
+	"github.com/nobe4/gh-not/internal/notifications"
+)
+
+type Runner struct{}
+
+func (_ *Runner) Run(n *notifications.Notification, tags []string, w io.Writer) error {
+	slog.Debug("tagging notification", "notification", n.Id, "tags", tags)
+
+	tagsToAdd := []string{}
+	tagsToRemove := []string{}
+
+	for _, tag := range tags {
+		switch tag[0] {
+		case '+':
+			tagsToAdd = append(tagsToAdd, tag[1:])
+		case '-':
+			tagsToRemove = append(tagsToRemove, tag[1:])
+		default:
+			tagsToAdd = append(tagsToAdd, tag)
+		}
+	}
+
+	n.Meta.Tags = append(n.Meta.Tags, tagsToAdd...)
+
+	newTags := []string{}
+	for _, tag := range n.Meta.Tags {
+		if !slices.Contains(tagsToRemove, tag) {
+			newTags = append(newTags, tag)
+		}
+	}
+
+	// Ensure tags are sorted and unique
+	slices.Sort(newTags)
+	n.Meta.Tags = slices.Compact(newTags)
+
+	fmt.Fprint(w, colors.Red("TAGGED ")+n.String())
+
+	return nil
+}
---
+++ internal/actions/tag/tag_test.go
@@ -0,0 +1,68 @@
+package tag
+
+import (
+	"slices"
+	"strings"
+	"testing"
+
+	"github.com/nobe4/gh-not/internal/notifications"
+)
+
+func TestRun(t *testing.T) {
+	r := &Runner{}
+	n := &notifications.Notification{}
+	w := &strings.Builder{}
+
+	tests := []struct {
+		name string
+		tags []string
+		args []string
+		want []string
+	}{
+		{
+			name: "empty",
+		},
+		{
+			name: "no new tag",
+			tags: []string{"tag0", "tag1"},
+			want: []string{"tag0", "tag1"},
+		},
+		{
+			name: "add tags",
+			tags: []string{"tag0", "tag1"},
+			args: []string{"+tag2", "tag3"},
+			want: []string{"tag0", "tag1", "tag2", "tag3"},
+		},
+		{
+			name: "remove tags",
+			tags: []string{"tag0", "tag1"},
+			args: []string{"-tag1", "-tag2"},
+			want: []string{"tag0"},
+		},
+		{
+			name: "add and remove tags",
+			tags: []string{"tag0", "tag1", "tag2"},
+			args: []string{"-tag1", "tag3", "tag2"},
+			want: []string{"tag0", "tag2", "tag3"},
+		},
+		{
+			name: "handle duplicates",
+			tags: []string{"tag0", "tag1", "tag2"},
+			args: []string{"+tag1", "+tag1", "+tag2", "-tag2", "-tag2", "tag3", "-tag3"},
+			want: []string{"tag0", "tag1"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			n.Meta.Tags = test.tags
+			if err := r.Run(n, test.args, w); err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+
+			if !slices.Equal(n.Meta.Tags, test.want) {
+				t.Errorf("expected tags to be %v, got %v", test.want, n.Meta.Tags)
+			}
+		})
+	}
+}
