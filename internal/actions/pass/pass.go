d67c13d33bb2c7418bcf1c2059001555f3a519c6
Joakim Uddholm <joakim.uddholm@einride.tech>
2025-12-22 12:33:30 +0100

    feat: make pass action log to show some output

--- internal/actions/pass/pass.go
+++ internal/actions/pass/pass.go
@@ -4,13 +4,16 @@ Package pass implements an [actions.Runner] that does nothing.
 package pass
 
 import (
+	"fmt"
 	"io"
 
+	"github.com/nobe4/gh-not/internal/colors"
 	"github.com/nobe4/gh-not/internal/notifications"
 )
 
 type Runner struct{}
 
-func (*Runner) Run(_ *notifications.Notification, _ []string, _ io.Writer) error {
+func (*Runner) Run(n *notifications.Notification, _ []string, w io.Writer) error {
+	fmt.Fprint(w, colors.Blue("PASS ")+n.String())
 	return nil
 }
